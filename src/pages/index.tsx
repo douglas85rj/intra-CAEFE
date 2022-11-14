eb9ab3efa55e3417c54c4329b08e1f92bee7235c
douglas85rj <douglas85rj@gmail.com>
2022-11-13 21:03:28 -0300

    New_index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,16 @@
-import React from "react";
-import { Layout } from "../layout/Layout";
-import { ProfileHeader } from "../components/ProfileHeader";
-import { Feed } from "../components/Feed";
+import * as React from 'react'
+import { Link } from 'gatsby'
 
-const items = Array.from({ length: 10 }, (_, index) => ({
-  title: `Title ${index}`,
-  link: `https://google.com/${index}`,
-  image: "https://picsum.photos/200/300",
-}));
-
-export default function Home() {
+const IndexPage = () => {
   return (
-    <Layout>
-      <header>
-        <ProfileHeader
-          avatar="https://i.ibb.co/0J6zpk4/logo-caefe-novo.png"
-          bio="Lorem ipsufgndsgiof didfgmjidfogmdfg Lorem ipsufgndsgiof didfgmjidfogmdfg Lorem ipsufgndsgiof didfgmjidfogmdfg Lorem ipsufgndsgiof didfgmjidfogmdfg"
-          link="https://google.com"
-          name="Núcleo de Tecnologia"
-          publishCount={10}
-          role="Analista de sistemas"
-          username="Douglas Souza"
-        />
-      </header>
-      <div className="feed-container">
-        <Feed items={items} />
-      </div>
-    </Layout>
-  );
+    <main>
+      <h1>Welcome to my Gatsby site!</h1>
+      <Link to="/about">About</Link>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </main>
+  )
 }
+
+export const Head = () => <title>Home Page</title>
+
+export default IndexPage
